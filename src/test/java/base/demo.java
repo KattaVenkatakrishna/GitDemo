7eea28ad24373f46810006be18c4616891c35e5c
Krishna <krishnakatta3194@gmail.com>
2025-11-11 01:26:54 +0530

    final commit by asian development team

---
+++ src/test/java/base/demo.java
@@ -0,0 +1,15 @@
+package base;
+
+import org.testng.annotations.Test;
+
+public class demo {
+	@Test
+	public void explicitwait2() {
+		System.out.println("wait for a few mintues!");
+	}
+	@Test
+	public void explicitwait3() {
+		System.out.println("you are almost done");
+	}
+
+}
